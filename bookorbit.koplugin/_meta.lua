dc3b4b3a7e9bb48af7bdc6b91da35d4bc78bc537
chaulagainrupak <rupak.chaulagainO19@gmail.com>
2026-05-21 13:03:36 +0545

    feature - added login screen to handle login details and store them login flow will be implemented once api is created

--- bookorbit.koplugin/_meta.lua
+++ bookorbit.koplugin/_meta.lua
@@ -1,5 +1,5 @@
 return {
-    name = "myplugin",
-    fullname = "My Plugin",
-    description = "Does cool shit",
+    name = "bookorbit",
+    fullname = "BookOrbit",
+    description = "KoReader plugin for syncing books and reading statistics to BookOrbit web-ui",
 }
